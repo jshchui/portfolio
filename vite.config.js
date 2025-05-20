dc819cebb0ef56015e850aeb9b41f5cbf97e6e3d
jshchui <jshchui@gmail.com>
2025-05-20 00:42:31 -0700

    changed configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',     // <-- your repository name here
+  base: '/portfolio/',     // <-- your repository name here
   plugins: [react()],
 })
